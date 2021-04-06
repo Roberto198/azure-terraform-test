a903c3f196bdca660c19c9970854bd2cc160140b
robertclegg1 <robc@apadmi.com>
2021-04-06 14:27:30 +0100

    added a function app, slight modification to app service plan to remove errors in az console re: storage. removed string interpolation as per depreciation warnings

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ resource "azurerm_app_service_plan" "consumptionServicePlan" {
   name                = "${var.environment}-functionplan-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "FunctionApp"
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Dynamic"
@@ -29,7 +30,22 @@ resource "azurerm_app_service_plan" "consumptionServicePlan" {
 
 resource "azurerm_application_insights" "appInsights" {
   name                = "${var.environment}-VMS"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  application_type    = "Web"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_function_app" "client" {
+  name                          = "${var.environment}-functionApp-VMS"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  app_service_plan_id           = azurerm_app_service_plan.consumptionServicePlan.id
+  storage_account_name          = azurerm_storage_account.functionsStorageAccount.name
+  storage_account_access_key    = azurerm_storage_account.functionsStorageAccount.primary_access_key
+  os_type                       = "linux"
+
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.appInsights.instrumentation_key
+  }
 }
