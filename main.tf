6a60a04fc50d71190d7dce3c3f537ec378d79b07
robertclegg1 <robc@apadmi.com>
2021-04-06 13:29:53 +0100

    Added app service plan. deploys fine.

--- main.tf
+++ main.tf
@@ -13,3 +13,23 @@ resource "azurerm_storage_account" "functionsStorageAccount" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+# create an app service plan
+resource "azurerm_app_service_plan" "consumptionServicePlan" {
+  name                = "${var.environment}-functionplan-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_application_insights" "appInsights" {
+  name                = "${var.environment}-VMS"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  application_type    = "Web"
+}
