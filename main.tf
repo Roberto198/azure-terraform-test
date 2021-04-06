9b123271d88b613b3338d1fb938a522ed6bd2743
robertclegg1 <robc@apadmi.com>
2021-04-06 12:13:22 +0100

    created a storage account for functions. Deploys fine

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+
+# creaate a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.environment}-vehicle-rg"
+  name     = "${var.environment}-VMS-rg"
   location = "West Europe"
+}
+
+# create a storaage account
+resource "azurerm_storage_account" "functionsStorageAccount" {
+  name                     = "${var.environment}vms"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
